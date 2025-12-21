8faa0f87d4f94000dd1bf565419e2c981f44aeb8
Jason Lee (Laptop) <jasomslee@gmail.com>
2025-12-20 22:07:31 -0600

    updated strumsense link

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -313,7 +313,7 @@ const projects: Project[] = [
       frontend: [PROJECT_SKILLS.next, PROJECT_SKILLS.react, PROJECT_SKILLS.ts],
       backend: [PROJECT_SKILLS.python, PROJECT_SKILLS.flask, PROJECT_SKILLS.docker],
     },
-    live: "https://strumsense-dzjp7boao-jaye1ees-projects.vercel.app",
+    live: "strumsense.vercel.app",
     github: "https://github.com/leechee/StrumSense",
     get content() {
       return (
