3ca5aa649332133d52776378a0a61608bce0551a
Jason <jasomslee@gmail.com>
2025-12-28 22:54:23 -0600

    vercel deploy issue

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
 import { DiMongodb, DiNginx, DiNpm, DiPostgresql, DiVim } from "react-icons/di";
 import {
   FaAws,
